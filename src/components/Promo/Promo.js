4f2dcf22f7833211a1a1d15abdb4e273af8c2404
vovitolog <vovitolog@gmail.com>
2022-08-28 13:52:16 +0300

    feat: добавить компонент Promo

---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,11 @@
+import "./Promo.css";
+
+export function Promo() {
+  return (
+    <section className="promo">
+    <h1 className="promo__header">
+      Учебный проект студента факультета Веб-разработки.
+    </h1>
+    </section>
+  );
+}
