517b034d4b172d0175a559918bbee9b31e1821da
vovitolog <vovitolog@gmail.com>
2022-08-28 11:09:27 +0300

    feat: добавить компонент Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
-import logo from '../../logo.svg';
-import './App.css';
-import { Logo } from '../Logo/Logo';
+import "./App.css";
+import { Header } from "../Header/Header";
 
 function App() {
   return (
     <div className="App">
-      ttetet
-      {/* <Logo/> */}
+      <Header />
     </div>
   );
 }
