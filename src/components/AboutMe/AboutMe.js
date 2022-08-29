5d8d56feaa3763c55ea75344b2e88cc60d4a62f3
vovitolog <vovitolog@gmail.com>
2022-08-29 16:11:05 +0300

    feat: добавить компонент AboutMe

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import "./AboutMe.css";
+import { SectionHeader } from "../SectionHeader/SectionHeader";
+import photo from "../../images/student.jpg";
+
+export function AboutMe() {
+  return (
+    <section className="about-me">
+      <SectionHeader title="Студент" />
+      <div className="about-me__wrapper">
+        <div className="about-me__profile">
+          <h1 className="about-me__profile-title">Владимир</h1>
+          <p className="about-me__profile-subtitle">
+            Фронтенд-разработчик, 37 лет
+          </p>
+          <p className="about-me__profile-info">
+            Решил перейти в сферу разработки так как хочу создавать полезные и
+            удобные для людей продукты. Фронтенд привлекает тем, что сразу виден
+            результат твоей работы. Ранее имел опыт создания сайтов на
+            конструкторах, но всегда хотелось иметь полный контроль над
+            написанием кода. Последний год активно развиваюсь как
+            frontend-разработчик. Увлекаюсь бегом, люблю открывать новые места
+            во время пробежек в путешествиях.
+          </p>
+          <ul className="about-me__profile-links">
+            <li>
+              <Link
+                to={{ pathname: "https://github.com/vovitolog" }}
+                className="header__button"
+                target="_blank"
+              >
+                Github
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={{ pathname: "https://career.habr.com/vovitolog" }}
+                className="header__button"
+                target="_blank"
+              >
+                Хабр карьера
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <img className="about-me__photo" src={photo} alt="Фото студента" />
+      </div>
+    </section>
+  );
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { Header } from "../Header/Header";
 import { Promo } from "../Promo/Promo";
 import { AboutProject } from "../AboutProject/AboutProject";
 import { Techs } from "../Techs/Techs";
+import {AboutMe} from "../AboutMe/AboutMe"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Promo />
       <AboutProject />
       <Techs />
+      <AboutMe/>
     </div>
   );
 }
