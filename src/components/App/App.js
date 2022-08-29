0c7171307a3ccabdac952d5d4ca708429b38dde7
vovitolog <vovitolog@gmail.com>
2022-08-29 18:31:05 +0300

    feat: добавить компонент Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,9 @@ import { Header } from "../Header/Header";
 import { Promo } from "../Promo/Promo";
 import { AboutProject } from "../AboutProject/AboutProject";
 import { Techs } from "../Techs/Techs";
-import {AboutMe} from "../AboutMe/AboutMe"
-import {Portfolio} from "../Portfolio/Portfolio"
+import { AboutMe } from "../AboutMe/AboutMe";
+import { Portfolio } from "../Portfolio/Portfolio";
+import { Footer } from "../Footer/Footer";
 
 function App() {
   return (
@@ -13,8 +14,9 @@ function App() {
       <Promo />
       <AboutProject />
       <Techs />
-      <AboutMe/>
-      <Portfolio/>
+      <AboutMe />
+      <Portfolio />
+      <Footer />
     </div>
   );
 }
