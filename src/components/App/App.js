bec56e3d39b69ab6a3043cb0a83bd3d81b2a2c45
vovitolog <vovitolog@gmail.com>
2022-08-29 17:00:08 +0300

    feat: добавить компонент Portfolio

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import { Promo } from "../Promo/Promo";
 import { AboutProject } from "../AboutProject/AboutProject";
 import { Techs } from "../Techs/Techs";
 import {AboutMe} from "../AboutMe/AboutMe"
+import {Portfolio} from "../Portfolio/Portfolio"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <AboutProject />
       <Techs />
       <AboutMe/>
+      <Portfolio/>
     </div>
   );
 }
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import "./Portfolio.css";
+
+export function Portfolio() {
+  return (
+    <section className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__list">
+        <li className="portfolio__list-item">
+          <Link
+            className="portfolio__link"
+            to={{ pathname: "https://github.com/vovitolog/how-to-learn" }}
+            target="_blank"
+          >
+            <p className="portfolio__link-text">Статичный сайт</p>
+          </Link>
+          <div className="portfolio__link-icon" />
+        </li>
+        <li className="portfolio__list-item">
+          <Link
+            className="portfolio__link"
+            to={{ pathname: "https://github.com/vovitolog/russian-travel" }}
+            target="_blank"
+          >
+            <p className="portfolio__link-text">Адаптивный сайт</p>
+          </Link>
+          <div className="portfolio__link-icon" />
+        </li>
+        <li className="portfolio__list-item">
+          <Link
+            className="portfolio__link"
+            to={{ pathname: "https://github.com/vovitolog/mesto" }}
+            target="_blank"
+          >
+            <p className="portfolio__link-text">Одностраничное приложение</p>
+          </Link>
+          <div className="portfolio__link-icon" />
+        </li>
+      </ul>
+    </section>
+  );
+}
