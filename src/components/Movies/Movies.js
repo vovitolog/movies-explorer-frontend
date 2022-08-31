e5c7f96817b546f8e39936dbafaa9dc6b666645a
vovitolog <vovitolog@gmail.com>
2022-08-31 13:47:19 +0300

    feat: добавить компонент SearchForm

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,11 +1,15 @@
 import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
+import {SearchForm} from "../SearchForm/SearchForm"
 import "./Movies.css";
 
 export function Movies() {
   return (
     <>
       <Header />
+      <main className="movies">
+      <SearchForm />
+      </main>
       <Footer />
     </>
   );
