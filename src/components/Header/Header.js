7a81049e551b791ace2d57dcba35de52ec220c1f
vovitolog <vovitolog@gmail.com>
2022-09-01 12:48:29 +0300

    feat: добавить адаптив в Header и Promo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,23 @@
 import { Logo } from "../Logo/Logo";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 export function Header() {
   return (
     <header className="header">
       <Logo />
+      <ul className="header__links">
+        <li className="header__links-item">
+          <Link className="header__link transition-link" to="/signup">
+            Регистрация
+          </Link>
+        </li>
+        <li className="header__links-item">
+        <Link className="header__button transition-button" to="/signin">
+          Войти
+        </Link>
+        </li>
+      </ul>
     </header>
   );
 }
