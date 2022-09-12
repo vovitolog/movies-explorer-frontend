870f55fd395846fee62984c18c3d01c2dcc6728f
vovitolog <vovitolog@gmail.com>
2022-09-12 21:49:41 +0300

    feat: добавить адаптив компонента Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,9 +6,9 @@ export function Portfolio() {
     <section className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
-        <li className="portfolio__list-item">
+        <li className="portfolio__list-item ">
           <Link
-            className="portfolio__link"
+            className="portfolio__link transition-link"
             to={{ pathname: "https://github.com/vovitolog/how-to-learn" }}
             target="_blank"
           >
@@ -18,7 +18,7 @@ export function Portfolio() {
         </li>
         <li className="portfolio__list-item">
           <Link
-            className="portfolio__link"
+            className="portfolio__link transition-link"
             to={{ pathname: "https://github.com/vovitolog/russian-travel" }}
             target="_blank"
           >
@@ -28,7 +28,7 @@ export function Portfolio() {
         </li>
         <li className="portfolio__list-item">
           <Link
-            className="portfolio__link"
+            className="portfolio__link transition-link"
             to={{ pathname: "https://github.com/vovitolog/mesto" }}
             target="_blank"
           >
